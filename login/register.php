55a556f31e72b53475dfecc2b71a7934ccba26f5
Cheuk Man Fung <>
2014-12-29 11:49:16 +0000

    Register page and client-side checking updated

--- login/register.php
+++ login/register.php
@@ -4,33 +4,43 @@
     <meta charset="utf-8">
   <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
     <title>Register | Clicker2GO</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
   </head>
 
   <body class="homepage">
   <ul>
          <form class="white" action="register_check.php" method="POST">
             <li> <font size=10>Register</font></li>
-		      <input type="text" name="username" placeholder="Username" class="name" required />
-		      <input type="password" name="password" id="pw1" placeholder="Password" onkeyup="validate()" class="email" required />
-		      <input type="password" name="repeat password" id="pw2" placeholder="Password" class="email" required /><span id="check"></span>
-		      <input type="text" name="name" placeholder="name" class="email" required />
+		      <input type="text" name="name" placeholder="Name" id="name" class="name" onKeyUp="validate();"required />
+		      <input type="text" name="username" placeholder="Username" id="username" class="name" onKeyUp="validate();" required />
+		      <input type="password" name="password" id="password1" placeholder="Password" class="email" onKeyUp="validate();" required />
+		      <input type="password" name="password2" id="password2" placeholder="Re-type Password" class="email" onKeyUp="validate();" required />
+            <p id="check" class="error"></p>
+            <input type="submit" name="submit" id="submit" class="btn shadow animate grey" value="Register" disabled />
 
-            <input type="submit" name="submit" id="submit" class="btn shadow animate green" value="register" />
-         
          </form>
  <script>
           function validate() {
-              var pw1 = document.getElementById("pw1").value;
-              var pw2 = document.getElementById("pw2").value;
-              if(pw1 == pw2) {
-                  document.getElementById("check").innerHTML="<font color='green'>same password input</font>";
-                  document.getElementById("submit").disabled = false;
+              var name = document.getElementById("name").value != "";
+              var username = document.getElementById("username").value != "";
+              var pw1 = document.getElementById("password1").value;
+              var pw2 = document.getElementById("password2").value;
+              var password = pw1 != "";
+              if(pw1 != pw2) {
+                document.getElementById("check").innerHTML="Passwords do not match!";
+                document.getElementById("submit").disabled = true;
+                document.getElementById("submit").className="btn shadow animate grey";
               }
               else {
-                  document.getElementById("check").innerHTML="<font color='red'>different password input</font>";
-                document.getElementById("submit").disabled = true;
-              }
+                if(name && username && password) {
+		  document.getElementById("check").innerHTML="";
+                  document.getElementById("submit").disabled = false;
+                  document.getElementById("submit").className="btn shadow animate green";
+                }
+                else {
+                  document.getElementById("submit").className="btn shadow animate grey";
+                }
+             }
           }
       </script>
   </body>
