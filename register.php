9f5db5e9e83959f8aa6c8b23445c4117ee60d0a0
Xinzi Shen <>
2015-01-28 02:39:31 +0000

    untested,correct little mistake

--- register.php
+++ register.php
@@ -11,9 +11,9 @@
   <ul>
          <form class="white" action="login_check.php" method="POST">
             <li> <font size=14>Register</font></li>
-		      <input type="text" name="username" id="pw1" placeholder="Username" class="name" required />
-		      <input type="password" name="password" id="pw2" placeholder="Password" onkeyup="validate()" class="email" required />
-		      <input type="password" name="repeat password" placeholder="Password" class="email" required /><span id="check"></span>
+		      <input type="text" name="username" placeholder="Username" class="name" required />
+		      <input type="password" name="password" id="pw1" placeholder="Password" onkeyup="validate()" class="email" required />
+		      <input type="password" name="repeat password" id="pw2" placeholder="Password" class="email" required /><span id="check"></span>
 		      <input type="text" name="name" placeholder="name" class="email" required />
 
             <input type="submit" name="submit" class="btn shadow animate green" value="register" />
@@ -24,11 +24,11 @@
               var pw1 = document.getElementById("pw1").value;
               var pw2 = document.getElementById("pw2").value;
               if(pw1 == pw2) {
-                  document.getElementById("check").innerHTML="<font color='green'>same input</font>";
+                  document.getElementById("check").innerHTML="<font color='green'>same password input</font>";
                   document.getElementById("submit").disabled = false;
               }
               else {
-                  document.getElementById("check").innerHTML="<font color='red'>different inpur</font>";
+                  document.getElementById("check").innerHTML="<font color='red'>different password input</font>";
                 document.getElementById("submit").disabled = true;
               }
           }
