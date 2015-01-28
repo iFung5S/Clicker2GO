d62fe66ce0c351112e01befdf5338b1df23d1034
Cheuk Man Fung <>
2015-01-28 18:16:37 +0000

    Updated login

--- login/login.php
+++ login/login.php
@@ -12,12 +12,16 @@
          <form class="white" action="login_check.php" method="POST">
 
             <li> <img class="center" src="images/logo.png" width="70" height="70" /> CLICKER2GO</li>
-            <br>
-		      <input type="text" name="username" placeholder="Username" class="name" required />
+            <br />
+		      <input type="text" name="username" id="username" placeholder="Username" class="name" required />
 		      <input type="password" name="password" placeholder="Password" class="email" required />
-
+           <p> <?php if($_GET['err'] == 1) {echo "<span class='error'>Incorrect username or password.</span>";}
+                     if($_GET['logout'] == 1 {echo "<span class='correct'>Logout successfully.</span>";} ?> </p>
             <input type="submit" name="submit" class="btn shadow animate green" value="Login" />
          
          </form>
+    <script>
+      document.getElementById('username').select();
+    </script>
   </body>
 </html>
