be1b0213b706fabe351892586696b451318ba4ed
Cheuk Man Fung <>
2015-01-28 18:04:43 +0000

    Updated login mechanism

--- login/login_check.php
+++ login/login_check.php
@@ -3,46 +3,40 @@
   <head>
     <meta charset="utf-8">
   <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-    <title>Login message | Clicker2GO</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Login... | Clicker2GO</title>
+    <link rel="stylesheet" href="./style.css">
   </head>
   <body class="homepage">
+
   <?php
 
-    include_once('config.inc.php');
+    include_once('./config.inc.php');
 
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $conn = mysqli_connect($database_host, $database_user, $database_pass,$group_dbnames[0]);
 
-    $userValidate = "SELECT * FROM user 
-    WHERE username='$username'";  
+    $userValidate = "SELECT * FROM user WHERE username='$username'";  
 
     $search = mysqli_query($conn, $userValidate);
      
-    if (mysqli_num_rows($search)>0)
+    if (mysqli_num_rows($search) > 0)
+    {
+      $row = mysqli_fetch_assoc($search);
+      if ($password == $row['password'])
       {
-       $row = mysqli_fetch_assoc($search);
-
-       if ($password == $row["password"])
-         {
-          echo "<script>location.href='./temp_home.html';</script>"; 
-         }
-       else
-         {
-           echo "Wrong password";
-           echo '<br>';
-           echo '<a href="./login.php">Back to the login page</a>';  
-         }
+        echo "<script>window.location.assign('./temp_home.html');</script>";
+        $_SESSION['username'] = $username;
+        $_SESSION['name'] = $row['name'];
       }
+      else
+      {
+        echo "<script>window.location.assign('login.php?err=1');</script>";  
+      }
+    }
     else 
-     {
-       echo "User not exist";
-       echo '<br>';
-       echo '<a href="./login.php">Back to the login page</a>';
-       
-      }
-
+    {
+      echo "<script>window.location.assign('login.php?err=1');</script>";
+    }
    
     mysqli_close($conn);
   
