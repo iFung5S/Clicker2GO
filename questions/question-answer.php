f04c5e250b895cce1379d4e184d5ebe9d1e4679b
Paris Mitsides <>
2015-02-02 12:59:29 +0000

    added loop to create questions

--- questions/question-answer.php
+++ questions/question-answer.php
@@ -14,11 +14,13 @@
 
       // get question data
       $result = mysqli_query($conn, "SELECT * FROM `questions` WHERE id=$qid");
-      $result_array = mysqli_fetch_assoc($result);
-      $question = $result_array["question"];
-      $answer1 = $result_array["answer1"];
+      $result_row = mysqli_fetch_assoc($result);
+      $question = $result_row["question"]; // question is column with index 3
+
+      
       // get answers in form and echo
-    ?>
+   ?>
+   
   <body>
     <p>
       <?= "$question" ?>
@@ -26,9 +28,18 @@
     <br/>
     <form>
       <ol>
-        <li>
-          <input name="answer1" type="radio" value="1"/> <?= "$answer1" ?>
-        </li>
+        <?php
+        // use index j for the number of answer
+        for ($i=1; $i<=6; $i++) // answers are from index 4 to 9 
+        {
+          if (!empty($result_row['answer' . $i])) // only print if value not NULL
+          {
+            echo "<li>
+                    <input name='answer' type='radio' value='$i' /> $result_row['answer'.$i]
+                  </li>";
+          }
+        }
+        ?>
       </ol>
     </form>
   </body>
