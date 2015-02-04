67b979f1c51d3fb90aa133b568aaf26af6290cb4
Xinzi Shen <>
2015-02-04 22:55:01 +0000

    try conn

--- dbCon.php
+++ dbCon.php
@@ -5,4 +5,5 @@ ORM::configure('mysql:host=$database_host;dbname=$group_dbnames[0]');
 ORM::configure('username', $database_user);
 ORM::configure('password', $database_pass);
 ORM::configure('return_result_sets', true);
+$db = ORM::get_db();
 ?>
