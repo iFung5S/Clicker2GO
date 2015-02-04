78e5b29a613d197b4db5d632cd21a395470f9a00
Xinzi Shen <>
2015-02-04 23:01:57 +0000

    try conn

--- dbCon.php
+++ dbCon.php
@@ -1,7 +1,7 @@
 <?php
 include('config.inc.php');
 require_once 'idiorm.php';
-ORM::configure('mysql:host=$database_host;dbname=$group_dbnames[0]');
+ORM::configure('mysql:host=dbhost.cs.man.ac.uk;dbname=2014_comp10120_x4');
 ORM::configure('username', $database_user);
 ORM::configure('password', $database_pass);
 ORM::configure('return_result_sets', true);
