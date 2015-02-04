cefa09ddf4c96df38ce991178291b469a9f77ee6
Xinzi Shen <>
2015-02-04 01:42:51 +0000

    create question

---
+++ questions/createQuestion.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<?php
+// Initialize session
+session_start();
+
+// Jump to login page if username not set
+if (!isset($_SESSION['username'])) {
+        header('Location: ../login/login.php');
+}
+
+ $courseName = $_POST['courseName'];
+ $date = $_POST['date'];
+  include_once ('../sqlconnect.php');
+?>
+  <head>
+    <meta charset="utf-8">
+  <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <title>Create Question | Clicker2GO</title>
+    <link rel="stylesheet" href="../style.css">
+  </head>
+
+  <body class="homepage">
+      <div>
+      <span><a href='../index.php'>Home</a></span>>
+      <span><a href="datePage.php?courseName=<?php echo $courseName; ?>"><?php echo $courseName; ?></a></span>>
+      <span><a href="questionlist.php?date=<?php echo $date; ?>&courseName=<?php echo $courseName; ?>"><?php echo $date; ?></span>>
+      <span>Create Question</span></div>
+
+    <form method="POST" action="saveQuestion.php">
+      <div><span name="courseName"><?php echo $courseName; ?></span>>
+           <span name="date"><?php echo $date; ?></span></div>
+      <div>Question:</div>
+      <textarea rows="5" cols="50" name="question" autofocus required></textarea></br>
+      <div>answer 1:</div>
+      <input type="text" name="answer1" size="50" autofocus required>
+      <input type="checkbox" name="correct[]" value="answer1" id="answer1"/>
+      <label for="answer1">correct</label></br>
+      <div>answer 2:</div>
+      <input type="text" name="answer2" size="50" autofocus required>
+      <input type="checkbox" name="correct[]" value="answer2" id="answer2"/>
+      <label for="answer2">correct</label></br>
+      <div>answer 3:</div>
+      <input type="text" name="answer3" size="50">
+      <input type="checkbox" name="correct[]" value="answer3" id="answer3"/>
+      <label for="answer3">correct</label></br>
+      <div>answer 4:</div>
+      <input type="text" name="answer4" size="50">
+      <input type="checkbox" name="correct[]" value="answer4" id="answer4"/>
+      <label for="answer4">correct</label></br>
+      <div>answer 5:</div>
+      <input type="text" name="answer5" size="50">
+      <input type="checkbox" name="correct[]" value="answer5" id="answer5"/>
+      <label for="answer5">correct</label></br>
+      <div>answer 6:</div>
+      <input type="text" name="answer6" size="50">
+      <input type="checkbox" name="correct[]" value="answer6" id="answer6"/>
+      <label for="answer6">correct</label></br>
+      <input type="submit" class="button" value="CREATE"/> 
+    </form>
+
+  </body>
+</html>
---
+++ questions/saveQuestion.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<?php
+// Initialize session
+session_start();
+
+// Jump to login page if username not set
+if (!isset($_SESSION['username'])) {
+        header('Location: ../login/login.php');
+}
+  $courseName = $_POST['courseName'];
+  $date = $_POST['date'];
+  $question = $_POST['question'];
+  for($i = 1;$i <= 6;$i++){
+    $answer[$i] = $_POST['answer'.$i];
+   }
+  if(isset($_POST['correct'])){
+    $correct = $_POST['correct'];
+    $correct = implode("|",$correct);
+    }
+
+?>
+  <head>
+    <meta charset="utf-8">
+  <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <title>Creating | Clicker2GO</title>
+    <link rel="stylesheet" href="../style.css">
+  </head>
+
+  <body class="homepage">
+  <?php
+
+    include('../sqlconnect.php');
+
+    $sql = "INSERT INTO question (course, date, question, answer1, answer2, answer3, answer4, answer5, answer6, correct) VALUES ('$course','$date','$question','$answer[1]','$answer[2]','$answer[3]','$answer[4]','$answer[5]','$answer[6]','$correct')";
+
+    if (mysqli_query($conn, $sql))
+     {
+      echo "<script>window.location.assign('questionlist.php');</script>";
+     }
+    else
+     {
+     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+ 
+?>
+  </body>
+</html>
