48929dceab5ec5b341f9b75fb8426bb8bdd2c069
Xinzi Shen <>
2015-02-05 20:01:56 +0000

    change SQL to idiorm

--- questions/timer.php
+++ questions/timer.php
@@ -2,18 +2,18 @@
 
 session_start();
 
-include('../sqlconnect.php');
+include_once ('../dbCon.php');
 
 $id = 1; // Just hardcoded 1 for now, should get from embedding page
 
-$query = "SELECT starttime, endtime FROM questions WHERE id='$id'";
-
-$result = mysqli_query($conn, $query);
-$row = mysqli_fetch_assoc($result);
-if ($row['starttime'] != NULL)
+$time_s_e = ORM::for_table('questions')
+            ->select('starttime')
+            ->select('endtime')
+            ->find_one($id);
+if (!empty($time_s_e->starttime))
 {
   $time = time();
-  $endtime = strtotime($row['endtime']);
+  $endtime = strtotime($time_s_e->endtime);
 
   $countdown = $endtime - $time;
   
