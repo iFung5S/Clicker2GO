20964f06a37f667e87b90492066b00c75595317e
Aivaras <>
2015-02-27 11:58:21 +0000

    changing hp31

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (!isset($_SESSION['uid'])) {
         $confirm = "javascript:if(confirm('Do you want to delete course unit $courseName? (Also related questions)'))location='questions/removeCourse.php?courseName=$courseName'";
       }
 
-      $list_course = $list_course . "<li><a href='questions/datePage.php?courseName=$courseName'>$courseName</a> <a href='javascript:void(0)' style='font-size:408px;color:red;text-decoration:none;' onclick=\"$confirm\">x</a></li> <br> <div id=rectangle> </div> <br>";
+      $list_course = $list_course . "<li><a href='questions/datePage.php?courseName=$courseName'>$courseName</a> <a href='javascript:void(0)' style='font-size:28px;color:red;text-decoration:none;' onclick=\"$confirm\">x</a></li> <br> <div id=rectangle> </div> <br>";
       }
   }
 
