28308f77a6ff19d08dbb3708311c4cc0d9684023
Xinzi Shen <>
2015-02-27 15:13:48 +0000

    rectangle

--- questions/datePage.php
+++ questions/datePage.php
@@ -44,7 +44,7 @@ if(isset($_GET['courseName'])){
       }
       else if ($_SESSION['type'] == 'Student' && time()<strtotime($min_time))
       {
-        $date_list = $date_list."<li><a href='#'>$date</a><span style='font-size:12px;color:grey;'>  (Not Start)</span></li>"; 
+        $date_list = $date_list."<li><a href='#'>$date</a><span style='font-size:12px;color:grey;'>  (Not Start)</span></li> <br> <div id=rectangle> </div> <br>"; 
       }
       else
       {
