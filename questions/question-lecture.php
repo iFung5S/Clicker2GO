feb6b37c2e008ce61a4c19f73299458636e3c435
Paris Mitsides <>
2015-03-02 11:48:41 +0000

    question-lecture check if lecturer

--- questions/question-lecture.php
+++ questions/question-lecture.php
@@ -12,66 +12,81 @@
   else
   {
     $uid = $_SESSION['uid'];
+    // connect to mysql
+    include_once ('../lib/dbCon.php');
+    // check if user is a lecturer (user type = 2)
+    $is_lecturer = ORM::for_table('u_type')
+                        ->where(array(
+                               'uid'=>$uid,
+                               'id_t'=>2
+                         ))
+                        ->find_one();
+    // check if he is the onwer of the question(when we make changes to database)
+    // $is_owner
   }
 
-if(isset($_GET['qid'])) {
-  // connect to mysql
-  include_once ('../lib/dbCon.php');
-  if (isset($_GET['seq'])) {
-    $seq = $_GET['seq'];
-  } else { $seq = ""; }
+  if(isset($_GET['qid']))
+  {
+
+    if (isset($_GET['seq']))
+      $seq = $_GET['seq'];
+    else
+      $seq = "";
 
     $qid = $_GET['qid'];
+    if (empty($is_lecturer))
+      header("Location: question-answer.php?seq=$seq&qid=$qid");
+
     $question_row = ORM::for_table('questions')-> find_one($qid);
 
- if (!empty($question_row)) {
+    if (!empty($question_row)) {
 
-    $cuid = $question_row->id_cu;
-    $courseName = ORM::for_table('course_units')->find_one($cuid)->course;
-    $date = $question_row->date;
+      $cuid = $question_row->id_cu;
+      $courseName = ORM::for_table('course_units')->find_one($cuid)->course;
+      $date = $question_row->date;
 
-    $correct_answer = explode("|",$question_row-> correct);
-    $question = $question_row-> question;
-    $numbering_characters="ABCDEF";
-    $answers = "";
+      $correct_answer = explode("|",$question_row-> correct);
+      $question = $question_row-> question;
+      $numbering_characters="ABCDEF";
+      $answers = "";
 
-    for ($i=1; $i<=6; $i++) 
-    {
-      
-      $answer = $question_row->get('answer'.$i); 
-      if (!empty($answer)) 
+      for ($i=1; $i<=6; $i++)
       {
-        $isCorrect = "";
-        if (in_array('answer'.$i, $correct_answer))
-          $isCorrect = "<span style='font-size:12px;color:green;'>  (Correct)</span>";  
-        $N=$numbering_characters[$i-1];
-        
-        $answers = $answers .
-                    "<li>$N. $answer  $isCorrect</li>";
+
+        $answer = $question_row->get('answer'.$i);
+        if (!empty($answer))
+        {
+          $isCorrect = "";
+          if (in_array('answer'.$i, $correct_answer))
+            $isCorrect = "<span style='font-size:12px;color:green;'>  (Correct)</span>";
+          $N=$numbering_characters[$i-1];
+
+          $answers = $answers .
+                      "<li>$N. $answer  $isCorrect</li>";
+        }
       }
-    }
 
-  include('graph.php');
-  $comment = "<iframe src='comment.php?qid=$qid' seamless></iframe>";
-  $placeholder = array("##graph##", "##question##", "##answers##",
-                 "##comment##","##courseName##","##date##","##qnumber##","##name##");
-  $replace = array($graph, $question, $answers,$comment,$courseName,$date
-                     ,$seq,$_SESSION['name']);
-  echo str_replace($placeholder, $replace, file_get_contents('question-lecture'));
+    include('graph.php');
+    $comment = "<iframe src='comment.php?qid=$qid' seamless></iframe>";
+    $placeholder = array("##graph##", "##question##", "##answers##",
+                   "##comment##","##courseName##","##date##","##qnumber##","##name##");
+    $replace = array($graph, $question, $answers,$comment,$courseName,$date
+                       ,$seq,$_SESSION['name']);
+    echo str_replace($placeholder, $replace, file_get_contents('question-lecture'));
 
-//for errors
- }
- else {
-  $information = "The question with id '$qid' is not exist.";
-  $placeholder = array("##information##","##name##");
-  $replace = array($information,$_SESSION['name']);
-  echo str_replace($placeholder, $replace, file_get_contents('error'));
- }
-}
-else {
-  $information = "The question id is empty.";
-  $placeholder = array("##information##","##name##");
-  $replace = array($information,$_SESSION['name']);
-  echo str_replace($placeholder, $replace, file_get_contents('error'));
-}
+  //for errors
+   }
+   else {
+    $information = "The question with id '$qid' is not exist.";
+    $placeholder = array("##information##","##name##");
+    $replace = array($information,$_SESSION['name']);
+    echo str_replace($placeholder, $replace, file_get_contents('error'));
+   }
+  }
+  else {
+    $information = "The question id is empty.";
+    $placeholder = array("##information##","##name##");
+    $replace = array($information,$_SESSION['name']);
+    echo str_replace($placeholder, $replace, file_get_contents('error'));
+  }
 ?>
