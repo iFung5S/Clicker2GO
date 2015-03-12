325a7a888919666d5cfd170ea899dc097502b57e
Cheuk Man Fung <>
2015-03-12 23:17:08 +0000

    Removed Home Logo

--- login/switch_type.php
+++ login/switch_type.php
@@ -1,25 +1,24 @@
 <?php
   session_start();
-  
+
   if (!isset($_SESSION['uid'])) {
         header('Location: ../');
   }
-  
+
   include ('../lib/dbCon.php');
-  
+
   if (isset($_POST['ready']) && $_POST['ready'] == 1)
   {
-    if (count($_SESSION['type']) > 2 
-        || (count($_SESSION['type']) == 2
+    if (count($_SESSION['type']) > 2 || (count($_SESSION['type']) == 2
             && !in_array(array("type"=>"Administrator"),$_SESSION['type'])))
     {
       if (in_array(array("type"=>"Student"),$_SESSION['type']))
-      {  $_SESSION['type'][0]['type'] = "Student (inactive)";  
+      {  $_SESSION['type'][0]['type'] = "Student (inactive)";
          $_SESSION['type'][1]['type'] = "Lecturer";}
       else if (in_array(array("type"=>"Lecturer"),$_SESSION['type']))
-      {  $_SESSION['type'][1]['type'] = "Lecturer (inactive)"; 
+      {  $_SESSION['type'][1]['type'] = "Lecturer (inactive)";
          $_SESSION['type'][0]['type'] = "Student";}
-    
+
     $type = "";
     foreach ($_SESSION['type'] as $u_type)
       $type = $type.$u_type['type']."<br>";
